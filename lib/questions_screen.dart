0e95a4842d8129b07c8b4e52853e69dec486d299
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-06-16 22:40:06 +0300

    Fixes and ndk updates

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/answer_button.dart';
-class QuestionsScreen extends StatefulWidget{
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionScreenState();
@@ -12,27 +15,44 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion(){
+  void answerQuestion() {
     setState(() {
       currentQuestionIndex++;
     });
   }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    // Check if all questions are answered
+    if (currentQuestionIndex >= questions.length) {
+      return Center(
+        child: Text(
+          'You completed the quiz!',
+          style: GoogleFonts.lato(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+
     final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24), // Add space from edges
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
+              style: GoogleFonts.lato(
                 color: Colors.white,
-                fontSize: 20,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
@@ -40,9 +60,7 @@ class _QuestionScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
                 answerText: answer,
-                onTap: () {
-                  answerQuestion();
-                },
+                onTap: answerQuestion,
               );
             }),
           ],
